1c1048d95a504e778eba3d9e48c7d368c7196951
eexcalibur <slnazhangtao@163.com>
2021-02-02 11:17:38 -0800

    Update run_DNS.sh
    2.2

--- faster/run_DNS.sh
+++ faster/run_DNS.sh
@@ -10,5 +10,5 @@ ulimit -s unlimited
 #source /home/tzhang/.zshrc
 export LD_LIBRARY_PATH=/software/libjpeg/lib:/software/openmpi/1.6.5-intel/lib:/software/hdf4/hdf4/lib:/software/intel/lib/intel64:$LD_LIBRARY_PATH
 
-cd /home/thang/DNS/climate/
+cd /home/tzhang/DNS/climate/
 /software/openmpi/1.6.5-intel/bin/mpirun  --mca btl openib,self -np 64 ./climate -d 3 -p 4 4 4 -i input-cmw-test/in-cmw-test-zt -o out-cmw
